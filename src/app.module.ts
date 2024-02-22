479a88f1c5a5e275ff0325a105e71e52758117ae
Alan Diniz <alanzinid@gmail.com>
2024-02-22 11:43:53 -0300

    feat: add conection in database postgres

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local']
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      // entities: [], 
+      // synchronize: true,
+    }),
+    UserModule
+  ],
+  controllers: [],
+})
+export class AppModule { }
